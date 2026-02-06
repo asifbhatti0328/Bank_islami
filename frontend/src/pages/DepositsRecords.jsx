27679126ddae2c42efe2590cac5fea12f6ee5fa1
asifbhatti0328 <asifbhattiwebdeveloper@gmail.com>
2026-02-06 22:17:36 +0500

    nkn,m.nllmnnn

--- frontend/src/pages/DepositsRecords.jsx
+++ frontend/src/pages/DepositsRecords.jsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 const DepositsRecords = () => {
   const [show,setshow]= useState(false)
   return (
-    <div className='py-[70px] bg-black/5 p-4'>
+    <div className='py-[70px]'>
+      <div className='bg-black/5 p-4 rounded'>
       <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-2 mb-8'>
 
       </div>
@@ -15,14 +16,14 @@ const DepositsRecords = () => {
 
       </div>
 
-
-
-      <div className='text-center'>
+      </div>
+            <div className='text-center'>
         {show ?         
         <h1  className='text-1xl font-bold'>No More data.</h1>:
         <h1 className='text-1xl font-bold'>Data Not found.</h1>
          }
       </div>
+
     </div>
   )
 }
