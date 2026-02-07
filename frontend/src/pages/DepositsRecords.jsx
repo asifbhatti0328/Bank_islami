625f30d924f4f7f6b12fa2be305e3a041d184aac
asifbhatti0328 <asifbhattiwebdeveloper@gmail.com>
2026-02-07 22:13:42 +0500

    nmmnnnm

--- frontend/src/pages/DepositsRecords.jsx
+++ frontend/src/pages/DepositsRecords.jsx
@@ -12,36 +12,36 @@ const DepositsRecords = () => {
           <i class="fa fa-chevron-left mr-[4rem] ml-[1rem] font-bold text-black" aria-hidden="true"></i>
         </NavLink>
 
-        <h1 className='text-[1.2rem] font-bold text-black'>Deposit Records</h1>
+        <h1 className='text-[1.1rem] font-bold text-black'>Deposit Records</h1>
       </div>
 
 
-      <div className='bg-black/5 p-4 rounded'>
-        <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-2 mb-8'>
+      <div className='bg-black/5 p-2 rounded'>
+        <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-1 mb-8'>
           <div className='flex flex-col justify-evenly p-4'>
 
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Deposit Amount</p>
               <p className='font-bold text-yellow-300'>2500</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Bank Name</p>
               <p className='font-bold text-yellow-300'>JazzCash</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Accout Holder</p>
               <p className='font-bold text-yellow-300'>Muhammad Asif</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Account Number</p>
               <p className='font-bold text-yellow-300'>03286696596</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Date</p>
               <p className='font-bold text-yellow-300'>07 Feb 2026 10:59 AM</p>
             </div>
 
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Status</p>
               <p className='font-bold text-yellow-300'>Pending</p>
             </div>
@@ -50,31 +50,31 @@ const DepositsRecords = () => {
 
         </div>
 
-                <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-2 mb-8'>
+        <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-1 mb-8'>
           <div className='flex flex-col justify-evenly p-4'>
 
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Deposit Amount</p>
               <p className='font-bold text-yellow-300'>2500</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Bank Name</p>
               <p className='font-bold text-yellow-300'>JazzCash</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Accout Holder</p>
               <p className='font-bold text-yellow-300'>Muhammad Asif</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Account Number</p>
               <p className='font-bold text-yellow-300'>03286696596</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Date</p>
               <p className='font-bold text-yellow-300'>07 Feb 2026 10:59 AM</p>
             </div>
 
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Status</p>
               <p className='font-bold text-yellow-300'>Pending</p>
             </div>
@@ -83,32 +83,31 @@ const DepositsRecords = () => {
 
         </div>
 
-
-        <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-2 mb-8'>
+        <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-1 mb-8'>
           <div className='flex flex-col justify-evenly p-4'>
 
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Deposit Amount</p>
               <p className='font-bold text-yellow-300'>2500</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Bank Name</p>
               <p className='font-bold text-yellow-300'>JazzCash</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Accout Holder</p>
               <p className='font-bold text-yellow-300'>Muhammad Asif</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Account Number</p>
               <p className='font-bold text-yellow-300'>03286696596</p>
             </div>
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Date</p>
               <p className='font-bold text-yellow-300'>07 Feb 2026 10:59 AM</p>
             </div>
 
-            <div className='flex items-center justify-between px-2'>
+            <div className='flex items-center justify-between'>
               <p className='font-bold'>Status</p>
               <p className='font-bold text-yellow-300'>Pending</p>
             </div>
@@ -116,7 +115,6 @@ const DepositsRecords = () => {
           </div>
 
         </div>
-
 
 
 
