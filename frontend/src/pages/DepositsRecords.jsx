9374a38719b826e6a00a158be39d264324de3d4b
asifbhatti0328 <asifbhattiwebdeveloper@gmail.com>
2026-02-07 22:49:11 +0500

    nnnljmluilku

--- frontend/src/pages/DepositsRecords.jsx
+++ frontend/src/pages/DepositsRecords.jsx
@@ -7,7 +7,7 @@ const DepositsRecords = () => {
   return (
     <div className='py-[70px]'>
 
-      <div className='flex h-[2rem] pl-2 mb-5 w-[60%] items-center justify-between'>
+      <div className='flex h-[2rem] pl-2 mb-5 w-[80%] items-center justify-between'>
         <NavLink to={'/'}>
           <i class="fa fa-chevron-left mr-[4rem] ml-[1rem] font-bold text-black" aria-hidden="true"></i>
         </NavLink>
@@ -17,6 +17,43 @@ const DepositsRecords = () => {
 
 
       <div className='bg-black/5 p-2 rounded'>
+
+
+
+        <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-1 mb-8'>
+          <div className='flex flex-col justify-evenly p-4'>
+
+            <div className='flex items-center justify-between'>
+              <p className='font-bold'>Deposit Amount</p>
+              <p className='font-bold text-yellow-300'>2500</p>
+            </div>
+            <div className='flex items-center justify-between'>
+              <p className='font-bold'>Bank Name</p>
+              <p className='font-bold text-yellow-300'>JazzCash</p>
+            </div>
+            <div className='flex items-center justify-between'>
+              <p className='font-bold'>Accout Holder</p>
+              <p className='font-bold text-yellow-300'>Muhammad Asif</p>
+            </div>
+            <div className='flex items-center justify-between'>
+              <p className='font-bold'>Account Number</p>
+              <p className='font-bold text-yellow-300'>03286696596</p>
+            </div>
+            <div className='flex items-center justify-between'>
+              <p className='font-bold'>Date</p>
+              <p className='font-bold text-yellow-300'>07 Feb 2026 10:59 AM</p>
+            </div>
+
+            <div className='flex items-center justify-between'>
+              <p className='font-bold'>Status</p>
+              <p className='font-bold text-yellow-300'>Pending</p>
+            </div>
+
+          </div>
+
+        </div>
+
+
         <div className='bg-white border border-[1px solied black] w-full h-[200px] rounded p-1 mb-8'>
           <div className='flex flex-col justify-evenly p-4'>
 
@@ -115,6 +152,8 @@ const DepositsRecords = () => {
           </div>
 
         </div>
+
+
 
 
 
