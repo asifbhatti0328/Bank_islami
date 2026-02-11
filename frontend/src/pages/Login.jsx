312f68c94ce9ca4668063b20322fcfb1546e92d1
asifbhatti0328 <asifbhattiwebdeveloper@gmail.com>
2026-02-12 02:58:40 +0500

    m,mm,,fgrfg

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,10 +44,10 @@ const Login = () => {
     <div className='py-[70px]'>
       <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800' action="">
         <div className='inline-flex items-center gap-2 mb-2 mt-10'>
-          <p className='prata-regular text-black text-3xl'>Login</p>
+          <p className='prata-regular text-black text-3xl font-bold'>Login</p>
         </div>
-                 <div className='flex items-center justify-end p-1 w-full'>
-                   <h3 className='text-1xl font-bold'><i> فون نمبر درج کریں</i> </h3>
+                 <div className='flex items-center justify-end w-full'>
+                   <h3 className='text-1xl font-bold px-2'> فون نمبر درج کریں </h3>
                  </div>
         <TextField label="Phone" variant="outlined" onChange={(e) => setphone(e.target.value)} value={phone} className='w-full px-3 py-2' type="Phone" required />
         <TextField label="Password" variant="outlined" onChange={(e) => setpassword(e.target.value)} value={password} className='w-full px-3 py-2' type="password" required />
